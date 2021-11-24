69550989d89d0fb6de2aedb2f943f0ee7ea354fc
Stanciu Ruben <stanciu.ruben20@gmail.com>
2021-11-24 18:27:24 +0100

    RootReducer setup

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,4 +1,11 @@
 const initialState = {
+    userName: null,
     cart: [],
-    
+    orders: []
+}
+
+const RootReducer = ( state = initialState, action ) => {
+    switch ( action.payload ) {
+        default: return state;
+    }
 }
